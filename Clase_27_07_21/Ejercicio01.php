e3fda80c2ed26c708f578401e04aa2584049e10c
Jarr59 <rrjosearr@gmail.com>
2021-07-27 20:14:34 -0600

    "Practica ejercicio clase"

---
+++ Clase_27_07_21/Ejercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejercicio01</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>10 x 1 = 10</p>";
+            echo "<p>10 x 2 = 20 </p>";
+            echo " <p> 10 x 3 = 30 </p>";
+            echo " <p> 10 x 4 = 40 </p>";
+            echo " <p> 10 x 5 = 50 </p>";
+            echo " <p> 10 x 6 = 60 </p>";
+            echo " <p> 10 x 7 = 70 </p>";
+            echo " <p> 10 x 8 = 80 </p>";
+            echo " <p> 10 x 9 = 90 </p>";
+            echo " <p> 10 x 10 = 100 </p>";
+        ?>
+    </body>
+</html>   
