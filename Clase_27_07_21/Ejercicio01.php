64e4fb60d2eb2bf65615f2ca045c21a2aa0046c8
Jarr59 <rrjosearr@gmail.com>
2021-07-30 21:00:08 -0600

    "Ejercicio 01"

--- Clase_27_07_21/Ejercicio01.php
+++ Clase_27_07_21/Ejercicio01.php
@@ -5,16 +5,37 @@
     </head>
     <body>
         <?php
-            echo "<p>10 x 1 = 10</p>";
-            echo "<p>10 x 2 = 20 </p>";
-            echo " <p> 10 x 3 = 30 </p>";
-            echo " <p> 10 x 4 = 40 </p>";
-            echo " <p> 10 x 5 = 50 </p>";
-            echo " <p> 10 x 6 = 60 </p>";
-            echo " <p> 10 x 7 = 70 </p>";
-            echo " <p> 10 x 8 = 80 </p>";
-            echo " <p> 10 x 9 = 90 </p>";
-            echo " <p> 10 x 10 = 100 </p>";
+            $multplicando = 10;
+            $uno = 1;
+            $respuesta = $multplicando * $uno;
+            echo "<p>10 x 1 =   $respuesta </p>";
+            $dos = 2;
+            $respuesta = $multplicando * $dos;
+            echo "<p>10 x 2 =   $respuesta </p>";
+            $tres = 3;
+            $respuesta = $multplicando * $tres;
+            echo " <p> 10 x 3 = $respuesta </p>";
+            $cuatro = 4;
+            $respuesta = $multplicando * $cuatro;
+            echo " <p> 10 x 4 = $respuesta </p>";
+            $cinco = 5;
+            $respuesta = $multplicando * $cinco;
+            echo " <p> 10 x 5 = $respuesta </p>";
+            $seis = 6;
+            $respuesta = $multplicando * $seis;
+            echo " <p> 10 x 6 = $respuesta </p>";
+            $siete = 7;
+            $respuesta = $multplicando * $siete;
+            echo " <p> 10 x 7 = $respuesta </p>";
+            $ocho = 8;
+            $respuesta = $multplicando * $ocho;
+            echo " <p> 10 x 8 = $respuesta </p>";
+            $nueve = 9;
+            $respuesta = $multplicando * $nueve;
+            echo " <p> 10 x 9 = $respuesta </p>";
+            $diez = 10;
+            $respuesta = $multplicando * $diez;
+            echo " <p> 10 x 10= $respuesta </p>";
         ?>
     </body>
 </html>   
